d14ecacfa6c778019bc13e128cbffffeee844898
Maydel Rego <maidelrego@yahoo.com>
2025-05-27 14:06:35 -0500

    fix: update contact link to open email client for inquiries

--- src/App.vue
+++ src/App.vue
@@ -32,10 +32,13 @@
                   </p>
 
                   <a
-                    href="#"
+                    href="mailto:maidelrego@yahoo.com?subject=Inquiry%20from%20Portfolio"
+                    target="_blank"
+                    rel="noopener"
                     class="px-7 py-3 md:px-9 md:py-4 font-medium md:font-semibold bg-gray-700 text-gray-50 text-sm rounded-md hover:bg-gray-50 hover:text-gray-700 transition ease-linear duration-500"
-                    >Hire me</a
                   >
+                    Hire Me
+                  </a>
                 </div>
               </div>
             </div>
